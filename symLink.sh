38d745901fcd6e90963d2751287bcbf717062238
roofrack <robaylard@gmail.com>
2021-04-02 23:12:08 -0700

    apr3/21

--- symLink.sh
+++ symLink.sh
@@ -2,8 +2,10 @@
 
 # A fun little project to create sym links in yer home directory to yer dot files...
 #
-# Of course you will have to change the parts where it says bob and make it just
-# work for your home directory...
+# Of course you will have to change the parts where it says bob and make
+# it just work for your home directory...
+
+
 
 files=".bashrc .vimrc .inputrc .i3status.conf"
 dir=~/dotfiles
@@ -16,6 +18,9 @@ for file in $files; do
 done
 cd ~
 
+
+
+
 echo "
 -I used an f option (ln -sf in the link command to delete
 any files that may already exist.
@@ -23,5 +28,5 @@ any files that may already exist.
 -If the link will be nested in a directory such as ~/.config/i3/config it might
 be easier to just cd into the directory and use the ln -s command
 from there...
--so for example... cd into .config/i3/ and enter ln -sf ~/dotfiles/config
--and or perhaps cd .config/ranger/ and type ln -sf ~/dotfiles/rc.config"
+So for example... cd into .config/i3/ and enter ln -sf ~/dotfiles/config
+and or perhaps cd .config/ranger/ and type ln -sf ~/dotfiles/rc.conf"
