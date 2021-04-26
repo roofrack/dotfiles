ba107b8b73f36d590f4cf2b54205ee1ebe32ac01
roofrack <robaylard@gmail.com>
2021-04-26 05:19:37 -0700

    another

--- i3bootstrap.sh
+++ i3bootstrap.sh
@@ -3,7 +3,7 @@
 # A fun little project to create sym links in yer home directory to yer dot files...
 # This is for setting up a new install (Arch) with the I3 Window Manager and your config files.
 
-MESSAGE_1="Building and Sym-Linking"
+MESSAGE_1="Building directory and Sym-Linking for"
 MESSAGE_2="Sym-Linking"
 DIR_DOTFILES="$HOME/dotfiles/Config_Files"
 DIR_CONFIG="$HOME/.config"
@@ -17,7 +17,7 @@ DIR_WALLPAPER="$DIR_PICS/wallpaper"
 # ADD files which you want linked to your dotfiles
 # and then will need to add the stuff in below as well...
 # files_add="config terminalrc rc.conf arch3.png .bashrc .vimrc .inputrc .i3status.conf .fehbg .xinitrc picom.conf .tmux.conf"
-files_add="$DIR_DOTFILES/* $DIR_DOTFILES/.[!.]?*"
+files_add="$DIR_DOTFILES/* $DIR_DOTFILES/.[!.]?*"     # Used regex for hidden files here
 
 clear                       # Clear screen
 
@@ -29,23 +29,24 @@ for file in $files_add; do
 
     if [ $file == $DIR_DOTFILES/config ]; then
         mkdir -p $DIR_I3 && $SYM_LINK $DIR_I3
-        echo $MESSAGE_1 $DIR_I3/$file
+        # echo $MESSAGE_1 $DIR_I3/$file
+        echo $MESSAGE_1 $DIR_I3
 
     elif [ $file == $DIR_DOTFILES/terminalrc ]; then
         mkdir -p $DIR_XFCE4 && $SYM_LINK $DIR_XFCE4
-        echo $MESSAGE_1 $DIR_XFCE4/$file
+        echo $MESSAGE_1 $DIR_XFCE4
 
     elif [ $file == $DIR_DOTFILES/rc.conf ]; then
         mkdir -p $DIR_RANGER && $SYM_LINK $DIR_RANGER
-        echo $MESSAGE_1 $DIR_RANGER/$file
+        echo $MESSAGE_1 $DIR_RANGER
 
     elif [ $file == $DIR_DOTFILES/arch3.png ]; then
         mkdir -p $DIR_WALLPAPER && $SYM_LINK $DIR_WALLPAPER
-        echo $MESSAGE_1 $DIR_WALLPAPER/$file
-        echo
+        echo $MESSAGE_1 $DIR_WALLPAPER
+
     else
     $SYM_LINK $HOME
-    echo $MESSAGE_2 $file...
+    echo $MESSAGE_2 $file
     fi
 done
 
@@ -88,7 +89,7 @@ else
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-echo DONE!!!
+echo Finished !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo
-echo READ... $DIR_DOTFILES/README.md for more info
+echo READ...     dotfiles/README.md for more info
 echo
