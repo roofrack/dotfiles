c1812edcdf27acd75515165e6e261b389f7b947c
roofrack <robaylard@gmail.com>
2021-04-28 10:20:19 -0700

    another

--- i3bootstrap.sh
+++ i3bootstrap.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# A fun little project to create sym links in yer home directory to yer dot files...
-
 clear
 
 DIR_DOTFILES="$HOME/dotfiles/Config_Files"
@@ -12,39 +10,41 @@ DIR_XFCE4="$DIR_CONFIG/xfce4/terminal"
 DIR_RANGER="$DIR_CONFIG/ranger"
 DIR_WALLPAPER="$DIR_PICS/wallpaper"
 
+dir_build="$DIR_I3 $DIR_XFCE4 $DIR_RANGER $DIR_WALLPAPER"
 files_symlink="$DIR_DOTFILES/* $DIR_DOTFILES/.[!.]?*"
 
-#============================================================================================
-for file in $files_symlink; do
-
-# These variables have to be declared here or it won't sym-link. It reads in something up
-# above into $file instead of whats in the loop for file_add.
-sym_link="ln -sf $file"
+#=========================================================================================
+for dir in $dir_build; do
 message_dir="echo Building directory"
+    mkdir -p $dir
+    $message_dir $dir
+done
+#=========================================================================================
+
+#=========================================================================================
+for file in $files_symlink; do
+# These variables have to be declared here or it won't sym-link.
+sym_link="ln -sf $file"
 message_symlink="echo Sym-Linking $(basename $file)"
 
     if [ $file == $DIR_DOTFILES/config ]; then
-        mkdir -p $DIR_I3 && $sym_link $DIR_I3
-        $message_dir $DIR_I3
+        $sym_link $DIR_I3
 
     elif [ $file == $DIR_DOTFILES/terminalrc ]; then
-        mkdir -p $DIR_XFCE4 && $sym_link $DIR_XFCE4
-        $message_dir $DIR_XFCE4
+        $sym_link $DIR_XFCE4
 
     elif [ $file == $DIR_DOTFILES/rc.conf ]; then
-        mkdir -p $DIR_RANGER && $sym_link $DIR_RANGER
-        $message_dir $DIR_RANGER
+        $sym_link $DIR_RANGER
 
     elif [ $file == $DIR_DOTFILES/arch3.png ]; then
-        mkdir -p $DIR_WALLPAPER && $sym_link $DIR_WALLPAPER
-        $message_dir $DIR_WALLPAPER
+        $sym_link $DIR_WALLPAPER
 
     else
     $sym_link $HOME
     fi
     $message_symlink
 done
-#=============================================================================================
+#=========================================================================================
 
 # This part is just a decoration...
 # echo -ne "[ Installing Software  "
