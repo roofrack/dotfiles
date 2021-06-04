7b9ce31f41db667185bb73e1cfbff69882ecad8f
roofrack <robaylard@gmail.com>
2021-06-04 00:44:26 -0700

    another

--- SYM_LINK.sh
+++ SYM_LINK.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-# Add more directories below as needed.
 # Any config files added to DIR_DOTFILES will be symlinked when running this script.
+# Add more directories to be built below along with the name of the file.
 
 DIR_DOTFILES="$HOME/dotfiles/Config_Files"
 DIR_CONFIG="$HOME/.config"
 DIR_PICS="$HOME/Pictures"
 
-DIR_I3="$DIR_CONFIG/i3"
-DIR_XFCE4="$DIR_CONFIG/xfce4/terminal"
-DIR_RANGER="$DIR_CONFIG/ranger"
-DIR_PICOM="$DIR_CONFIG/picom"
-DIR_WALLPAPER="$DIR_PICS/wallpaper"
-
-DIR_BUILD=("$DIR_I3" "$DIR_XFCE4" "$DIR_RANGER" "$DIR_PICOM" "$DIR_WALLPAPER")
+declare -A DIR_BUILD=(
+    [config]=$DIR_CONFIG/i3
+    [terminalrc]=$DIR_CONFIG/xfce4/terminal
+    [rc.conf]=$DIR_CONFIG/ranger
+    [arch3.png]=$DIR_PICS/wallpaper
+    [picom.conf]=$DIR_CONFIG/picom
+    #[file]=directory
+    #[file]=directory
+)
 total_dirs="${#DIR_BUILD[@]}"
 FILES_SYMLINK="$DIR_DOTFILES/* $DIR_DOTFILES/.[!.]?*"           # Can't seem to make brace expan work.
 total_symlinks="$(( $(ls -al $DIR_DOTFILES | wc -l) - 3))"      # Must be a better way to calculate this
-count=0
 
 #-----------------------------------------------------------------------------------------
 # Function takes two arguments. $1 a message and $2 how many items will be iterated.
@@ -36,7 +37,6 @@ Progress_bar_message() {
     length_bar=${#bar}
     percent=$((100 % length_bar))
     length_percent="4"
-
     for i in $(seq $length_bar); do
         bar_timer
         number_of_spaces=$(($(tput cols) - ${#message_count} - ${#1} - $ab_length - $length_bar - 4 - $length_percent))
@@ -52,8 +52,9 @@ Progress_bar_message() {
 #=========================================================================================
 # Need to build directories for packages which install config files in nested dir's.
 #=========================================================================================
+count=0
 printf "\n"; tput civis
-for dir in "${DIR_BUILD[@]}"; do
+for dir in ${DIR_BUILD[@]}; do
     mkdir -p $dir
     message_dir="Building directory for $dir"
     Progress_bar_message "${message_dir}" "${total_dirs}"     # Calling the function with 2 arguments
@@ -65,19 +66,14 @@ printf "\n"
 #=========================================================================================
 count=0
 for file in $FILES_SYMLINK; do
-    sym_link="ln -sf $file"
+    if [[ -n ${DIR_BUILD[$(basename $file)]} ]]; then
+        ln -sf $file ${DIR_BUILD[$(basename $file)]}
+    else
+        ln -sf $file $HOME
+    fi
     message_symlink="Sym-Linking $(basename $file)"
-    case $file in
-        $DIR_DOTFILES/config) $sym_link $DIR_I3 ;;
-        $DIR_DOTFILES/terminalrc) $sym_link $DIR_XFCE4 ;;
-        $DIR_DOTFILES/rc.conf) $sym_link $DIR_RANGER ;;
-        $DIR_DOTFILES/arch3.png) $sym_link $DIR_WALLPAPER ;;
-        $DIR_DOTFILES/picom.conf) $sym_link $DIR_PICOM ;;
-        *) $sym_link $HOME ;;
-    esac
     Progress_bar_message "${message_symlink}" "${total_symlinks}"
 done
-sleep 0.7s
 
 #=========================================================================================
 
