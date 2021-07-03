2fac52d3a7b0c112e9abca8f962bdb71ca583650
roofrack <robaylard@gmail.com>
2021-07-02 21:26:48 -0700

    another

--- bootstrap_arch.sh
+++ bootstrap_arch.sh
@@ -5,6 +5,7 @@
 #                                                                                        =
 # <robaylard@gmail.com>                                                                  =
 #=========================================================================================
+
 clear
 banner_message(){
     message="WELCOME TO ARCH_BOOTSTRAP"
@@ -45,7 +46,7 @@ printf "\nFinished installing packages.\n"
 sleep 2
 
 
-# Sym-linking script
+# Building directories and sym-links using SYM_LINK script
 #=========================================================================================
 printf "\nSetting up directories and Sym-links...\n"
 sleep 2
@@ -73,25 +74,3 @@ end_message
 
 
 
-MIT License
-------------------------------
-Copyright (c) 2012-2021 Robert
-
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
