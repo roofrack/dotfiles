4c22555ecf8a3d406f25d6d9dfb1e7353749ef85
roofrack <robaylard@gmail.com>
2023-03-18 11:08:48 -0700

    another

--- startup_files/scripts/bash_commands.sh
+++ startup_files/scripts/bash_commands.sh
@@ -10,7 +10,10 @@ source $HOME/.tmux/plugins/tpm/bin/install_plugins
 # nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 # same thing only for lazy.vim
+printf "Configuring neovim\n"
 nvim --headless "+Lazy! sync" +qa
+printf "\nUse Mason to install lsp servers\n"
+nvim --headless +MasonUpdate +qa
 
 # TODO: Make it work to update treesitter from the command line here
 # nvim --headless -c 'TSUpdate | quitall'
