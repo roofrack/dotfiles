563337d25f88bf212f50b7e5819a6788db32c4cc
roofrack <robaylard@gmail.com>
2025-04-08 21:02:34 -0700

    another

--- bin/shell_scripts/google_chrome_updater/google_chrome_updater.sh
+++ bin/shell_scripts/google_chrome_updater/google_chrome_updater.sh
@@ -1,19 +1,20 @@
-# google-chrome... is a pain to update because it wont seem to update with
-# all these chrome files left in the system. This script will delete all these files
+# NOTE: google-chrome... is a pain to update because it won't seem to update with
+#       all these chrome files left in the system. This script will delete all these files
 
-# Running yay -S google-chrome 2> delete_these_chrome_files.txt
-# will put all the file names in a file named delete_these_chrome_files.txt
+# 1) Run the update command and send the errors to a file...
+# yay -S google-chrome 2> delete_these_chrome_files.txt
 
-# Will need to edit this list of files so only the file name is showing.
-# Need to write a script for this using awk or something.
-# or use vim macros... thats easy.
+# 2) Edit this file and remoove lines which don't have file names.
+#    Now use vim macros to edit each line (remove unwanted text)
+#    Should really make a command that does this automatically (maybe use awk?)
 
-# Then run yay google-chrome and it should update google-chrome just fine.
-
+# 3) run this script to remove all these unwanted files...
 for f in $(cat ./delete_these_chrome_files.txt); do
   if [[ -e "$f" ]]; then
     printf "%s deleted\n" "$f"
     sudo rm -rf "$f" >/dev/null 2>&1
   fi
 done
+
+# 4) Now the update command should work...
 # yay google-chrome
