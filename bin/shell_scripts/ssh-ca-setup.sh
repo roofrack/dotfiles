e6e64cf7b402903a8bfef764a708f0cdc249d634
roofrack <robaylard@gmail.com>
2026-03-05 21:06:07 -0800

    another

--- bin/shell_scripts/ssh-ca-setup.sh
+++ bin/shell_scripts/ssh-ca-setup.sh
@@ -3,8 +3,8 @@
 # Need to have password (or key) authorization enabled on host for this to initially be set up.
 # Allow root login in order to copy files over to host (server) (VeryBad! Change it back after setup!!).
 # Uncomment the functions in main() to run functions one at a time to ensure no errors.
-# I am using sshpass (pacman sshpass) here to automate some logins. I think thats ok in my local enviroment vm's.
 # Can really fine tune restrictions on certificates by using the principle field and setting a time limit.
+# If root is not added to user certificate principles field (-n) then can NOT ssh into server as root user.
 
 # Define a few variables...
 server="root@192.168.122.41"
@@ -15,8 +15,7 @@ host_key="host_key"
 ca_directory="$client_directory/ca"
 ca_user="ca_user"
 ca_host="ca_host"
-my_host_ca_configuration="20-my_ca.conf"
-fake_passwd="root" # avoid typing in password each time (just for this tutorial)
+my_host_ca_configuration="20-my_ca.conf" # NOTE: conf NOT config!!
 
 # 1. Create a Certificate Authority for signing public keys...
 # Setup a secret directory or place for the certificate authority (CA).
@@ -34,34 +33,32 @@ generate_user_keys() {
   ssh-keygen -f "$client_directory"/"$user_key" -C "My User Key"
 }
 
-# 3. Sign user_key.pub using the ca_host Certificate Authority...
+# 3. Sign user_key.pub using the Certificate Authority ca_user key...
 # Rob you need to remember that the principles field needs to be correct or leave blank
 # or it wont work. Remember how you spent hours figuring that out. Use correct user names here!
 sign_user_certificate() {
   printf '\n%s\n' "SIGNING USER PUBLIC KEY CERTIFICATE..."
-  ssh-keygen -s "$ca_directory"/"$ca_user" -I "client-machine" -n "root,rob" -V +22d "$client_directory"/"$user_key".pub
+  ssh-keygen -s "$ca_directory"/"$ca_user" -I "client-machine" -n "root,rob" -V +22d \
+    "$client_directory"/"$user_key".pub &>/dev/null
 }
 
 # 4. Configure the client...
 # Add the CA host public key to the ~/.ssh/known_hosts file for host auth (no more tofu).
 # The `*` here allows any host using this CA (I think).
-# NOTE: This has to run last as known_hosts file gets truncated with this function and it
-# erases the `root known host` thus preventing server commands from being able to ssh in.
 configure_client() {
   echo "@cert-authority * $(cat "$ca_directory"/"$ca_host".pub)" >"$client_directory"/known_hosts
 }
 
 # 5. Generate HOST Keys...
 # DO NOT put password on host key as sshd needs to access it!
-# Must allow root login on server for the server commands to work.
+# Must allow 'PermitRootLogin yes' on server for the server commands to work from the client.
 # Maybe just go into server directly and generate host keys there.
 # Cant seem to add a comment over the ssh wire for server host_key
 # but just go into the host_key file and edit the comment if desired.
-# CAN NOT use sshpass here as must log in once as root before using sshpass. I think maybe
-# because the known_hosts file needs to get updated with the root key first. I dunno?
 generate_host_keys() {
-  printf '\n%s\n' "LOGGING INTO HOST & GENERATING HOST KEY..."
-  ssh "$server" ssh-keygen -f "$server_directory"/"$host_key"
+  printf '\n%s\n' "LOGGING INTO HOST..."
+  printf '\n%s\n' "GENERATING HOST KEY..."
+  ssh -i "$user_key" "$server" ssh-keygen -f "$server_directory"/"$host_key"
 }
 
 # 6. Sign host_key.pub using the Certificate Authority...
@@ -72,56 +69,53 @@ generate_host_keys() {
 sign_host_certificate() {
   local hostname="192.168.122.41"
   printf '\n%s\n' "SIGNING HOST PUBLIC KEY CERTIFICATE..."
-  sshpass -p "$fake_passwd" scp "$server":"$server_directory"/"$host_key".pub "$client_directory"
-  ssh-keygen -h -s "$ca_directory"/"$ca_host" -I "host-machine" -n "$hostname" -V +22d "$client_directory"/"$host_key".pub
-  sshpass -p "$fake_passwd" scp "$client_directory"/"$host_key"-cert.pub "$server":"$server_directory"
+  scp -i "$user_key" "$server":"$server_directory"/"$host_key".pub "$client_directory" &>/dev/null
+  ssh-keygen -h -s "$ca_directory"/"$ca_host" -I "host-machine" -n "$hostname" -V +22d "$client_directory"/"$host_key".pub &>/dev/null
+  scp -i "$user_key" "$client_directory"/"$host_key"-cert.pub "$server":"$server_directory" &>/dev/null
   rm "$client_directory"/"$host_key".pub "$client_directory"/"$host_key"-cert.pub
 }
 
-# 7. Configure HOST... NOTE: THIS LOCKS DOWN THE SERVER TO NO LOGINS VIA PASSWORDS
+# 7. Configure HOST...
+# NOTE: THIS LOCKS DOWN THE SERVER - NO ROOT LOGINS if PermitRootLogin uncommented below.
 # Create configuration file (gets merged into sshd_config).
 # Copy configuration file from here to HOST:/etc/ssh/sshd_configd/.
 # Copy CA user_key.pub to server (allows host to trust the client user).
 # Restart the host sshd.
 # Delete my_host_ca_configuration file as not needed here.
-# NOTE: use the name conf NOT config here rob you dumbass.
 configure_host() {
   : >"$my_host_ca_configuration" # ': >' creates the file and truncates it
   cat <<EOF >>"$my_host_ca_configuration"
+  #PermitRootLogin no
   PasswordAuthentication no
   AuthenticationMethods publickey
+  
   
   TrustedUserCAKeys /etc/ssh/${ca_user}.pub
   HostKey /etc/ssh/${host_key}
   HostCertificate /etc/ssh/${host_key}-cert.pub
 EOF
-  sshpass -p "$fake_passwd" scp "$client_directory"/"$my_host_ca_configuration" "$server":"$server_directory"/sshd_config.d/
-  sshpass -p "$fake_passwd" scp "$ca_directory"/"$ca_user".pub "$server":"$server_directory"
-  sshpass -p "$fake_passwd" ssh "$server" systemctl restart sshd
+
+  printf '\n%s\n' "COPYING HOST CONFIG TO HOST..."
+  scp -i "$user_key" "$client_directory"/"$my_host_ca_configuration" \
+    "$server":"$server_directory"/sshd_config.d/ &>/dev/null
+  scp -i "$user_key" "$ca_directory"/"$ca_user".pub "$server":"$server_directory" &>/dev/null
+  ssh -i "$user_key" "$server" systemctl restart sshd &>/dev/null
   rm "$my_host_ca_configuration"
 }
 
-# Little helper function for...
-# Deletes my sshd_conf & restarts sshd.
-# Deleting my sshd_conf changes the sshd_conf back to default settings where
-# password logins ARE allowed. This is necessary to enable client to access the
-# host root /etc directory and make changes (add & copy files).
-# Why would I want to do this? For rapid testing ssh stuff inside my vm enviroment.
-# Can NOT use sshpass here (doesnt work)
-delete_ca_config() {
-  printf '\n%s\n' "DELETING... HOST:/etc/ssh/sshd_config.d/$my_host_ca_configuration"
-  ssh -q -t -i "$user_key" "$server" "rm /etc/ssh/sshd_config.d/$my_host_ca_configuration &> /dev/null; \
-      systemctl restart sshd &> /dev/null; echo RESTARTING sshd..."
+setup_ssh_agent() {
+  eval "$(ssh-agent -s)" &>/dev/null
+  ssh-add "$client_directory"/"$user_key" &>/dev/null
 }
 
 main() {
-  delete_ca_config
+  setup_ssh_agent
   # make_CA
   generate_user_keys
   sign_user_certificate
+  configure_client
   generate_host_keys
   sign_host_certificate
   configure_host
-  configure_client
 }
 main
